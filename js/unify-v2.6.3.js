518903fda6a4dd2f4b0b7c29109f345214c1a5c8
야근의 요정 <jokerx04@naver.com>
2023-06-14 01:55:47 +0900

    Update unify-v2.6.3.js

--- js/unify-v2.6.3.js
+++ js/unify-v2.6.3.js
@@ -2342,7 +2342,7 @@ $(function() {
 
 		global: false,
 		isCorsUrl: false,
-		url: 'https://cdn.jsdelivr.net/gh/jokerx04/jokerx04.com@65b5e00/json/menu.json',
+		url: 'https://cdn.jsdelivr.net/gh/jokerx04/jokerx04.com@8029f19/json/menu.json',
 		method: 'GET',
 		type: 'GET',
 		success: function (data, textStatus, jqXHR) {
